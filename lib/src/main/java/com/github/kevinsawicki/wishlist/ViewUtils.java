7be6a281271703de35a2dbc962dcf5853b31e019
Donn Felker <donn@donnfelker.com>
2013-03-28 15:59:23 -0700

    Adding increaseHitRect which will allow for easy upscaling of hit rects for various views. This is very useful if you have a tiny icon and need to make it easy to click/tap. Simply increase it by 10 (which defaults to dps) and the hit area will be increased.

--- lib/src/main/java/com/github/kevinsawicki/wishlist/ViewUtils.java
+++ lib/src/main/java/com/github/kevinsawicki/wishlist/ViewUtils.java
@@ -18,6 +18,10 @@ package com.github.kevinsawicki.wishlist;
 import static android.view.View.GONE;
 import static android.view.View.INVISIBLE;
 import static android.view.View.VISIBLE;
+
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.view.TouchDelegate;
 import android.view.View;
 
 /**
@@ -67,6 +71,49 @@ public class ViewUtils {
       }
     return view;
   }
+
+    /**
+     * Increases the hit rect of a view. This should be used when an icon is small and cannot be easily tapped on.
+     * Source: http://stackoverflow.com/a/1343796/5210
+     * @param amount The amount of dp's to be added to all four sides of the view hit purposes.
+     * @param delegate The view that needs to have its hit rect increased.
+     */
+    public void increaseHitRectBy(final int amount, final View delegate) {
+      increaseHitRectBy(amount, amount, amount, amount, delegate);
+    }
+
+    /**
+     * Increases the hit rect of a view. This should be used when an icon is small and cannot be easily tapped on.
+     * Source: http://stackoverflow.com/a/1343796/5210
+     * @param top The amount of dp's to be added to the top for hit purposes.
+     * @param left The amount of dp's to be added to the left for hit purposes.
+     * @param bottom The amount of dp's to be added to the bottom for hit purposes.
+     * @param right The amount of dp's to be added to the right for hit purposes.
+     * @param delegate The view that needs to have its hit rect increased.
+     */
+    public void increaseHitRectBy(final int top, final int left, final int bottom, final int right, final View delegate) {
+      final View parent = (View) delegate.getParent();
+        if(parent != null && delegate.getContext() != null) {
+        parent.post(new Runnable() {
+          // Post in the parent's message queue to make sure the parent
+          // lays out its children before we call getHitRect()
+          public void run() {
+            final float densityDpi = delegate.getContext().getResources().getDisplayMetrics().densityDpi;
+            final Rect r = new Rect();
+            delegate.getHitRect(r);
+            r.top -= transformToDensityPixel(top, densityDpi);
+            r.left -= transformToDensityPixel(left, densityDpi);
+            r.bottom += transformToDensityPixel(bottom, densityDpi);
+            r.right += transformToDensityPixel(right, densityDpi);
+            parent.setTouchDelegate(new TouchDelegate(r, delegate));
+          }
+        });
+      }
+    }
+
+    public int transformToDensityPixel(int regularPixel, float densityDpi) {
+        return ((int) (regularPixel * densityDpi));
+    }
 
   private ViewUtils() {
 
